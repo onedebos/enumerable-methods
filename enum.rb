7e5ca445604577414683cd45df1035d0a5e47bd9
adebola <onedebos@gmail.com>
2019-08-11 18:49:32 +0100

    create each-with-index method

--- enum.rb
+++ enum.rb
@@ -1,4 +1,5 @@
 module Enumerable
+=begin
     def my_each
         if block_given?
             i=0
@@ -13,7 +14,22 @@ module Enumerable
         end
         
     end
-    
+=end    
+
+def my_each_with_index
+    if block_given?
+        i=0
+        while(i<self.length)
+           yield(self[i],i) 
+           i+=1
+        end
+        self
+    else
+        self.to_enum
+    end
 end
 
-puts [1,2,3,4,5].my_each{|i| puts i}
+end
+
+
+[1,2,3,4,5].my_each_with_index{|e,index| puts e,index}
